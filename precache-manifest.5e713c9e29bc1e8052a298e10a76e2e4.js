72c004a8740ee69fe5aada5a873e240df012580b
Michelle Huang <michelle@freedompop.com>
2018-12-18 16:33:12 -0800

    Updates

---
+++ precache-manifest.5e713c9e29bc1e8052a298e10a76e2e4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/public/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4a2f574d04a485f8c063",
+    "url": "/public/static/css/main.1560cdae.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/public/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "758cc38d359e48a12d0e",
+    "url": "/public/static/js/1.758cc38d.chunk.js"
+  },
+  {
+    "revision": "c3ea8d972d62e65a19a9",
+    "url": "/public/static/js/runtime~main.c3ea8d97.js"
+  },
+  {
+    "revision": "4a2f574d04a485f8c063",
+    "url": "/public/static/js/main.4a2f574d.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/public/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/public/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/public/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "758cc38d359e48a12d0e",
+    "url": "/public/static/css/1.038caba7.chunk.css"
+  },
+  {
+    "revision": "0466fa779f20212dc7ccd121d101428b",
+    "url": "/public/index.html"
+  }
+];
